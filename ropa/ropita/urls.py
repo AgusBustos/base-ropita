6946d1924eebc63cddcc8620e78db24c68ad5cc7
AgusBustos <bustosagustin103@gmail.com>
2024-12-15 16:58:10 -0300

    abms y cabecera

--- ropa/ropita/urls.py
+++ ropa/ropita/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('abm_s/', views.ABM_Stock),
 ]
--- ropa/ropita/views.py
+++ ropa/ropita/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def ABM_Stock(request):
+    return render(request,'ABM_Stock.html')
